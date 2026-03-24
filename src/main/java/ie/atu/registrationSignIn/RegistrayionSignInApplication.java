95dcf9c581f14cb67aabaa9bba8c37c880897308
jose <g00463862@atu.ie>
2026-03-24 10:54:45 +0000

    first commit

---
+++ src/main/java/ie/atu/registrationSignIn/RegistrayionSignInApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.registrationSignIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrayionSignInApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrayionSignInApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/registrationSignIn/RegistrayionSignInApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.registrationSignIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrayionSignInApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
