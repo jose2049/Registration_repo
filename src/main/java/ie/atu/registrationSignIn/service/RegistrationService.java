f23a96ee2b23c465fb1aaaa16594c6fd05ad9dfd
AlvarezJoseManuel-ST <g00463862@atu.ie>
2026-04-15 17:22:56 +0100

    adding more code for the creation of the user + a way to check for time conflicts code added to get all users code added to get user by email code added to get user by student number

--- src/main/java/ie/atu/registrationSignIn/service/RegistrationService.java
+++ src/main/java/ie/atu/registrationSignIn/service/RegistrationService.java
@@ -2,6 +2,7 @@ package ie.atu.registrationSignIn.service;
 
 import ie.atu.registrationSignIn.module.Registration;
 import ie.atu.registrationSignIn.repository.RegistrationRepo;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,10 +17,30 @@ public class RegistrationService {
     }
     //Returning a created user
     public Registration addUser(Registration user){
+        //everything stored in users
+        users = registrationRepository.findAll();
 
+        //check for time conflicts
+        for(Registration existingUser: users){
+            //Same email
+            if(existingUser.getEmail().equals(user.getEmail())){
+                throw new RuntimeException("User with email already exists");
+            }
+        }
+        //Add info in the database
+        registrationRepository.save(user);
         return user;
     }
     //get all users
+    public List<Registration> getAllUsers() {
+        return registrationRepository.findAll();
+    }
     //get user by email
+    public List<Registration> getUserByEmail(String email) {
+        return registrationRepository.findByEmail(email);
+    }
     //get user by student number
+    public List<Registration> getUserByStudentNumber(String studentNumber) {
+        return registrationRepository.findByStudentNumber(studentNumber);
+    }
 }
